73a960d653bbae1017e0a5e459777b4860f5ed79
ianidi <51965970+ianidi@users.noreply.github.com>
2021-05-11 08:22:26 +0300

    update

--- src/mappings/lottery.ts
+++ src/mappings/lottery.ts
@@ -1,20 +1,20 @@
 import { log } from '@graphprotocol/graph-ts';
 import { Create } from '../types/Lottery/Lottery';
-import { Execution } from '../types/schema';
+import { Lottery } from '../types/schema';
 
 export function handleCreate(event: Create): void {
   log.debug('Parsing Create for txHash {}', [
     event.transaction.hash.toHexString(),
   ]);
   let txHash = event.transaction.hash;
-  let execution = Execution.load(txHash.toHexString());
-  if (execution == null) {
-    execution = new Execution(txHash.toHexString());
+  let lottery = Lottery.load(txHash.toHexString());
+  if (lottery == null) {
+    lottery = new Lottery(txHash.toHexString());
   }
-  execution.member = event.params.member;
-  execution.lotteryID = event.params.lotteryID;
-  execution.amount = event.params.amount;
-  execution.txHash = txHash;
-  execution.timestamp = event.block.timestamp;
-  execution.save();
+  lottery.member = event.params.member;
+  lottery.lotteryID = event.params.lotteryID;
+  lottery.amount = event.params.amount;
+  lottery.txHash = txHash;
+  lottery.timestamp = event.block.timestamp;
+  lottery.save();
 }
